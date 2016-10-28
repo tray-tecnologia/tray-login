05d1db5d5fc0e1bfd50345d35ad3d2c92a3f41c1
Douglas Neves <dsouza@tray.net.br>
2016-10-28 18:18:30 -0200

    Controle de visibilidade do botão fechar

--- src/tray-login.js
+++ src/tray-login.js
@@ -65,6 +65,7 @@ var trayLoginProto = {},
 
         this.bindValues('[data-element="tray-store"]', 'store');
         this.bindValues('[data-element="tray-session"]', 'session');
+        this.handleCloseButton();
     };
 
     /**
@@ -264,6 +265,17 @@ var trayLoginProto = {},
         return this;
     };
 
+    /**
+     * Open a screen and close others
+     * @return {object} trayLoginProto
+     */
+    trayLoginProto.handleCloseButton = function() {
+        this.closeButton = thatDoc.querySelector('.tray-close');
+
+        if (this.messages != null && this.messages.closeButton) {
+            this.closeButton.style.display = 'none';
+        }
+    };
     /**
      * When click in the close button
      * @return {object} trayLoginProto
