c8880654260c6e7342b137127d7d65c76c07eb55
Richard Santos <rsantos@tray.com.br>
2019-06-25 08:50:43 -0300

    commerce/central#150 - Modal inacessível (conflito layout)

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,95 @@
 <template>
-  <div id="tray-login" class="tray-login" ref="tray-login" v-show="showComponent">
-    <div class="tray-container">
-      <button class="tray-close"
-        @click="close">
-        X
+  <div class="tray-container" ref="tray-login" v-show="showComponent">
+    <button class="tray-close"
+      @click="close">
+      X
+    </button>
+    <app-identification id="identify" v-if="screen === 'Identification'"
+      class="tray-login-screens"
+      :callback="this.dataCallback"
+      :params="params">
+      <app-custom-texts v-if="hasCustomTexts"
+        :action="this.customTexts['main-action']"
+        slot="custom-texts">
+      </app-custom-texts>
+      <app-facebook-login v-if="facebookEnabled"
+        :callback="this.dataCallback"
+        :params="params"
+        slot="app-facebook-login">
+      </app-facebook-login>
+    </app-identification>
+
+    <app-login id="main" v-if="screen === 'Main'"
+      class="tray-login-screens"
+      :callback="this.dataCallback"
+      :params="params">
+      <app-custom-texts v-if="hasCustomTexts"
+        :error="this.customTexts['general-error-alert']"
+        :action="this.customTexts['main-action']"
+        slot="custom-texts">
+      </app-custom-texts>
+      <app-otp-button v-if="otpEnabled"
+        :callback="this.dataCallback"
+        :params="params"
+        slot="app-otp-login"
+      >
+      </app-otp-button>
+      <app-facebook-login v-if="facebookEnabled"
+        :callback="this.dataCallback"
+        :params="params"
+        slot="app-facebook-login">
+      </app-facebook-login>
+      <button type="button"
+        v-if="identificationEnabled"
+        class="tray-btn-default tray-btn-other-option"
+        @click="reset"
+        slot="app-back-step">
+        {{ $lang['go-back'] }}
       </button>
-      <app-identification id="identify" v-if="screen === 'Identification'"
-        class="tray-login-screens"
-        :callback="this.dataCallback"
-        :params="params">
-        <app-custom-texts v-if="hasCustomTexts"
-          :action="this.customTexts['main-action']"
-          slot="custom-texts">
-        </app-custom-texts>
-        <app-facebook-login v-if="facebookEnabled"
-          :callback="this.dataCallback"
-          :params="params"
-          slot="app-facebook-login">
-        </app-facebook-login>
-      </app-identification>
+    </app-login>
 
-      <app-login id="main" v-if="screen === 'Main'"
-        class="tray-login-screens"
-        :callback="this.dataCallback"
-        :params="params">
-        <app-custom-texts v-if="hasCustomTexts"
-          :error="this.customTexts['general-error-alert']"
-          :action="this.customTexts['main-action']"
-          slot="custom-texts">
-        </app-custom-texts>
-        <app-otp-button v-if="otpEnabled"
-          :callback="this.dataCallback"
-          :params="params"
-          slot="app-otp-login"
-        >
-        </app-otp-button>
-        <app-facebook-login v-if="facebookEnabled"
-          :callback="this.dataCallback"
-          :params="params"
-          slot="app-facebook-login">
-        </app-facebook-login>
-        <button type="button"
-          v-if="identificationEnabled"
-          class="tray-btn-default tray-btn-other-option"
-          @click="reset"
-          slot="app-back-step">
-          {{ $lang['go-back'] }}
-        </button>
-      </app-login>
+    <section id="blocked" v-if="screen === 'Blocked'"
+      class="tray-login-screens">
+      <div>
+        <strong class="tray-title tray-login__title">
+          {{ $lang['main-title']}}
+        </strong>
+        <p class="tray-action tray-error-message">
+          {{ $lang['blocked-user'] }}
+        </p>
+      </div>
+      <app-custom-texts v-if="hasCustomTexts"
+        :error="this.customTexts['general-error-alert']"
+        :action="this.customTexts['main-action']"
+        slot="custom-texts">
+      </app-custom-texts>
+      <p class="tray-action" v-else v-html="$lang['main-action']"></p>
+      <app-facebook-login v-if="facebookEnabled"
+        :callback="callback"
+        :params="params"
+        slot="app-facebook-login">
+      </app-facebook-login>
+      <button v-if="identificationEnabled"
+        class="tray-btn-default"
+        @click="reset"
+        slot="app-back-step">
+        {{ $lang['go-back'] }}
+      </button>
+    </section>
 
-      <section id="blocked" v-if="screen === 'Blocked'"
-        class="tray-login-screens">
-        <div>
-          <strong class="tray-title tray-login__title">
-            {{ $lang['main-title']}}
-          </strong>
-          <p class="tray-action tray-error-message">
-            {{ $lang['blocked-user'] }}
-          </p>
-        </div>
-        <app-custom-texts v-if="hasCustomTexts"
-          :error="this.customTexts['general-error-alert']"
-          :action="this.customTexts['main-action']"
-          slot="custom-texts">
-        </app-custom-texts>
-        <p class="tray-action" v-else v-html="$lang['main-action']"></p>
-        <app-facebook-login v-if="facebookEnabled"
-          :callback="callback"
-          :params="params"
-          slot="app-facebook-login">
-        </app-facebook-login>
-        <button v-if="identificationEnabled"
-          class="tray-btn-default"
-          @click="reset"
-          slot="app-back-step">
-          {{ $lang['go-back'] }}
-        </button>
-      </section>
-
-      <section id="tray-login-loading" v-show="loading"
-        :class="{
-          'tray-loading-hidden': !loading
-        }"
-        class="tray-loading">
-        <div class="tray-loading-mask">
-          <div class="tray-loading-line"></div>
-        </div>
-        <svg class="tray-loading-icon tray-icon-locked" viewBox="0 0 1024 1024">
-          <!-- eslint-disable-next-line -->
-          <path class="path1" d="M796.467 417.109v-132.642c0-155.58-128.956-284.467-284.467-284.467s-284.467 128.887-284.467 284.467v132.642c-64.444 0-113.801 49.289-113.801 113.801v379.29c0.068 64.444 49.289 113.801 113.801 113.801h568.866c64.444 0 113.801-49.289 113.801-113.801v-379.29c0.068-60.689-49.289-113.801-113.732-113.801zM265.489 284.399c0-136.533 109.978-246.511 246.511-246.511s246.511 109.978 246.511 246.511v132.71h-37.956v-132.71c0-113.801-94.822-208.623-208.623-208.623s-208.555 94.822-208.555 208.623v132.71h-37.956l0.068-132.71zM682.667 284.399v132.71h-341.333v-132.71c0-94.822 75.844-170.667 170.667-170.667s170.667 75.844 170.667 170.667zM872.311 568.798v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333c0 41.711-34.133 75.844-75.844 75.844h-568.866c-41.711 0-75.844-34.133-75.844-75.844v-379.221c0-41.711 34.133-75.844 75.844-75.844h568.866c41.711 0 75.844 34.133 75.844 75.844h-341.333v37.956l341.333-0.137z"></path>
-        </svg>
-      </section>
-    </div>
+    <section id="tray-login-loading" v-show="loading"
+      :class="{
+        'tray-loading-hidden': !loading
+      }"
+      class="tray-loading">
+      <div class="tray-loading-mask">
+        <div class="tray-loading-line"></div>
+      </div>
+      <svg class="tray-loading-icon tray-icon-locked" viewBox="0 0 1024 1024">
+        <!-- eslint-disable-next-line -->
+        <path class="path1" d="M796.467 417.109v-132.642c0-155.58-128.956-284.467-284.467-284.467s-284.467 128.887-284.467 284.467v132.642c-64.444 0-113.801 49.289-113.801 113.801v379.29c0.068 64.444 49.289 113.801 113.801 113.801h568.866c64.444 0 113.801-49.289 113.801-113.801v-379.29c0.068-60.689-49.289-113.801-113.732-113.801zM265.489 284.399c0-136.533 109.978-246.511 246.511-246.511s246.511 109.978 246.511 246.511v132.71h-37.956v-132.71c0-113.801-94.822-208.623-208.623-208.623s-208.555 94.822-208.555 208.623v132.71h-37.956l0.068-132.71zM682.667 284.399v132.71h-341.333v-132.71c0-94.822 75.844-170.667 170.667-170.667s170.667 75.844 170.667 170.667zM872.311 568.798v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333c0 41.711-34.133 75.844-75.844 75.844h-568.866c-41.711 0-75.844-34.133-75.844-75.844v-379.221c0-41.711 34.133-75.844 75.844-75.844h568.866c41.711 0 75.844 34.133 75.844 75.844h-341.333v37.956l341.333-0.137z"></path>
+      </svg>
+    </section>
   </div>
 </template>
 
