9038521c518a22cc5e43652d7a5295ca9a809e49
Mônica Colomera <monica.colomera@tray.net.br>
2020-07-02 16:21:57 -0300

    #220 - Fluxo de redefinição de senha

--- src/screens/Login/screens/RecoverPassword/screens/ConfirmCode.vue
+++ src/screens/Login/screens/RecoverPassword/screens/ConfirmCode.vue
@@ -1,8 +1,8 @@
 <template>
-  <form id="send-security-code"
-    class="tray-login__recover-password__confirm-code" method='POST' @submit.prevent="submit">
-    <input v-autofocus
+  <section id="send-security-code">
+    <input
       @keyup="$event.keyCode !== 13 ? clearErrors() : $event.preventDefault()"
+      v-on:load="this.load()"
       v-model="securityCode"
       type="text"
       id="password-code"
@@ -22,7 +22,7 @@
         <path class="path1" d="M796.467 417.109v-132.642c0-155.58-128.956-284.467-284.467-284.467s-284.467 128.887-284.467 284.467v132.642c-64.444 0-113.801 49.289-113.801 113.801v379.29c0.068 64.444 49.289 113.801 113.801 113.801h568.866c64.444 0 113.801-49.289 113.801-113.801v-379.29c0.068-60.689-49.289-113.801-113.732-113.801zM265.489 284.399c0-136.533 109.978-246.511 246.511-246.511s246.511 109.978 246.511 246.511v132.71h-37.956v-132.71c0-113.801-94.822-208.623-208.623-208.623s-208.555 94.822-208.555 208.623v132.71h-37.956l0.068-132.71zM682.667 284.399v132.71h-341.333v-132.71c0-94.822 75.844-170.667 170.667-170.667s170.667 75.844 170.667 170.667zM872.311 568.798v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333c0 41.711-34.133 75.844-75.844 75.844h-568.866c-41.711 0-75.844-34.133-75.844-75.844v-379.221c0-41.711 34.133-75.844 75.844-75.844h568.866c41.711 0 75.844 34.133 75.844 75.844h-341.333v37.956l341.333-0.137z"></path>
       </svg>
     </section>
-  </form>
+  </section>
 </template>
 
 <script>
@@ -126,6 +126,7 @@ export default {
       password: this.password,
       [this.identificationType]: this.identification,
     }) {
+      console.log(event)
       this.setLoading(true);
 
       if (!this.isValidSecurityCode) {
--- src/screens/Login/screens/RecoverPassword/screens/New.vue
+++ src/screens/Login/screens/RecoverPassword/screens/New.vue
@@ -213,3 +213,7 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+  @import '@/assets/sass/app.scss';
+</style>
