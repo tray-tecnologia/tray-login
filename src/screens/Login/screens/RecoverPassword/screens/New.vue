cb43462a8c0c54f6ede739d248661a713fccc941
Mônica Colomera <monica.colomera@tray.net.br>
2020-07-09 16:29:26 -0300

    #220 - Fluxo de redefinição de senha

--- src/screens/Login/screens/RecoverPassword/screens/New.vue
+++ src/screens/Login/screens/RecoverPassword/screens/New.vue
@@ -19,14 +19,26 @@
         {{ $lang['new-password-create'] }}
       </p>
     </div>
-    <app-confirm-code
-      :identification="identification"
-      :identificationType="identificationType"
-      :params="this.params"
-      v-model="securityCode"
-      :class="securityCodeClassses"
-      :password="this.password">
-    </app-confirm-code>
+    <fieldset class="tray-input-group">
+      <label :for="id">
+        <figure class="tray-input-icon" :class="inputClass">
+          <svg class="tray-icon-locked" viewBox="0 0 512 512">
+            <!-- eslint-disable-next-line -->
+            <path class="path1" d="M176 216h160c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16H176c-8.84 0-16 7.16-16 16v16c0 8.84 7.16 16 16 16zm-16 80c0 8.84 7.16 16 16 16h160c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16H176c-8.84 0-16 7.16-16 16v16zm96 121.13c-16.42 0-32.84-5.06-46.86-15.19L0 250.86V464c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V250.86L302.86 401.94c-14.02 10.12-30.44 15.19-46.86 15.19zm237.61-254.18c-8.85-6.94-17.24-13.47-29.61-22.81V96c0-26.51-21.49-48-48-48h-77.55c-3.04-2.2-5.87-4.26-9.04-6.56C312.6 29.17 279.2-.35 256 0c-23.2-.35-56.59 29.17-73.41 41.44-3.17 2.3-6 4.36-9.04 6.56H96c-26.51 0-48 21.49-48 48v44.14c-12.37 9.33-20.76 15.87-29.61 22.81A47.995 47.995 0 0 0 0 200.72v10.65l96 69.35V96h320v184.72l96-69.35v-10.65c0-14.74-6.78-28.67-18.39-37.77z"></path>
+          </svg>
+        </figure>
+      </label>
+      <input v-autofocus
+        @keyup="$event.keyCode !== 13 ? clearErrors() : $event.preventDefault()"
+        v-model="securityCode"
+        :id="id"
+        :class="inputClass"
+        class="tray-input"
+        :placeholder="$lang['otp-title']"/>
+      <small class="tray-feedbacks" v-show="errors.length">
+        <span class="tray-error-message" v-html="errors[errors.length - 1]"></span>
+      </small>
+    </fieldset>
     <app-toggle-password
       :state="errors.length >= 1 ? 'invalid' : 'valid'"
       v-model="passwordHandler"
@@ -73,7 +85,6 @@ import screenHandler from '@/mixins/screenHandler';
 import { mapState, mapActions } from 'vuex';
 import AppTogglePassword from '@/components/TogglePassword.vue';
 import AppConfirmPassword from '@/components/ConfirmPassword.vue';
-import AppConfirmCode from '@/screens/Login/screens/RecoverPassword/screens/ConfirmCode.vue';
 
 export default {
   name: 'AppNewPassword',
@@ -81,7 +92,6 @@ export default {
   components: {
     AppTogglePassword,
     AppConfirmPassword,
-    AppConfirmCode,
   },
   props: {
     endpoint: {
@@ -140,6 +150,19 @@ export default {
       return {
         'tray-input-invalid': this.errors.length >= 1,
       };
+    },
+
+    /**
+     * Verifica se o codigo de segurança preenchido é valido
+     * @return {boolean}
+     */
+    isValidSecurityCode() {
+      if (this.securityCode.length < 6) {
+        return false;
+      }
+
+      const onlyNumbersPattern = /^\d+$/;
+      return onlyNumbersPattern.test(this.securityCode);
     },
   },
   methods: {
@@ -200,13 +223,8 @@ export default {
         return;
       }
 
-      const validateSecurityCodeLen = this.securityCode >= 6;
-      const onlyNumbersPattern = /^\d+$/;
-      const isOnlyNumbers = onlyNumbersPattern.test(this.securityCode);
-
-      if (!validateSecurityCodeLen || !isOnlyNumbers) {
+      if (!this.isValidSecurityCode) {
         this.setError(this.$lang['invalid-code']);
-        this.setLoading(false);
         return;
       }
 
