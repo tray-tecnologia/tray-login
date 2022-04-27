e32a6cf31017ac04fc5c464083b79758f5bc8ece
Gustavo Andrade <gustavo.rodrigues@hubess.com.br>
2022-04-27 15:25:50 -0300

    removendo console

--- src/screens/Login/screens/Main.vue
+++ src/screens/Login/screens/Main.vue
@@ -250,11 +250,7 @@ export default {
           payloadPost.token = response.data.data.token;
           payloadPost.endpoint = this.payloadPostEndpoint;
 
-          console.log(this.callbackPost);
-          console.log(JSON.stringify(response.data));
-
           this.callbackLoginLayout(payloadPost).then((res) => {
-            console.log(JSON.stringify(res.data));
             const { token = '', redirect = '' } = res.data.data;
             this.redirect(redirect, token);
             return res;
