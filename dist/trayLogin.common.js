55026f6bce486a908f1b869bd77bc6383ed1a164
Gustavo Andrade <gustavo.rodrigues@hubess.com.br>
2022-05-13 16:34:40 -0300

    corrigindo caminho da url de callpost

--- dist/trayLogin.common.js
+++ dist/trayLogin.common.js
@@ -18863,12 +18863,12 @@ vue_runtime_esm["a" /* default */].use(vuex_esm["a" /* default */]);
   getters: getters_namespaceObject,
   strict: "production" !== 'production'
 }));
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"a9a82c9c-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/FacebookLogin.vue?vue&type=template&id=b2fd92c4&
-var FacebookLoginvue_type_template_id_b2fd92c4_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{ref:"facebook-button",staticClass:"tray-btn-facebook",on:{"click":function($event){_vm.$emitEvent.click('tray-login-facebook'), _vm.doFacebookLogin($event)}}},[_vm._v("\n    Facebook\n")])}
-var FacebookLoginvue_type_template_id_b2fd92c4_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"a9a82c9c-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/FacebookLogin.vue?vue&type=template&id=0376a510&
+var FacebookLoginvue_type_template_id_0376a510_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{ref:"facebook-button",staticClass:"tray-btn-facebook",on:{"click":function($event){_vm.$emitEvent.click('tray-login-facebook'), _vm.doFacebookLogin($event)}}},[_vm._v("\n    Facebook\n")])}
+var FacebookLoginvue_type_template_id_0376a510_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/FacebookLogin.vue?vue&type=template&id=b2fd92c4&
+// CONCATENATED MODULE: ./src/components/FacebookLogin.vue?vue&type=template&id=0376a510&
 
 // EXTERNAL MODULE: ./node_modules/@babel/runtime-corejs2/core-js/array/is-array.js
 var is_array = __webpack_require__("a745");
@@ -19109,7 +19109,7 @@ function FacebookLoginvue_type_script_lang_js_objectSpread(target) { for (var i 
      */
     urlCallbackPost: function urlCallbackPost() {
       localStorage.setItem('jwtToken', 'false');
-      return "".concat(document.location.origin).concat(this.homePath, "/login?").concat(this.urlParams);
+      return "".concat(document.location.origin).concat(this.pathEnvironment, "/login?").concat(this.urlParams);
     }
   }
 });
@@ -19225,8 +19225,8 @@ function normalizeComponent (
 
 var component = normalizeComponent(
   components_FacebookLoginvue_type_script_lang_js_,
-  FacebookLoginvue_type_template_id_b2fd92c4_render,
-  FacebookLoginvue_type_template_id_b2fd92c4_staticRenderFns,
+  FacebookLoginvue_type_template_id_0376a510_render,
+  FacebookLoginvue_type_template_id_0376a510_staticRenderFns,
   false,
   null,
   null,
