5e5b12f044828cc9e73a511875e9578d192d1661
Rayanne Bertolace Lima <rayanne.lima@tray.net.br>
2022-09-09 18:32:13 -0300

    add: facebook new btn

--- src/components/FacebookLogin.vue
+++ src/components/FacebookLogin.vue
@@ -2,8 +2,23 @@
   <button
     ref="facebook-button"
     class="tray-btn-facebook"
-    @click="$emitEvent.click('tray-login-facebook'), doFacebookLogin($event)">
-      Facebook
+    @click="$emitEvent.click('tray-login-facebook'), doFacebookLogin($event)"
+    @mouseover="removeIconColors = true"
+    @mouseleave="removeIconColors = false"
+  >
+    <figure class="tray-button-icon">
+      <svg width="10" height="18" viewBox="0 0 10 18">
+        <path
+          d="M9.18812 10.1249L9.68819 6.86749H6.56238V4.75361C6.56238 3.86261 6.99887
+          2.99355 8.39894 2.99355H9.81981V0.220426C9.81981 0.220426 8.53056 0.000488281
+          7.29756 0.000488281C4.72356 0.000488281 3.04112 1.56086 3.04112
+          4.38518V6.86805H0.179688V10.1255H3.04112V18.0005H6.56238V10.1255L9.18812 10.1249Z"
+          :fill="removeIconColors ? '#FFFFFF' : '#1877F2'"
+        />
+      </svg>
+    </figure>
+
+    <p>Facebook</p>
   </button>
 </template>
 
@@ -36,6 +51,12 @@ export default {
         };
       },
     },
+  },
+
+  data() {
+    return {
+      removeIconColors: false,
+    };
   },
 
   computed: {
