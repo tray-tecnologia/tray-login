a2f8eb22c2c65b070b53a94142c01531cf156584
Rayanne Bertolace Lima <rayanne.lima@tray.net.br>
2022-09-09 18:30:54 -0300

    fix: lint

--- src/components/TogglePassword.vue
+++ src/components/TogglePassword.vue
@@ -3,8 +3,12 @@
     <label :for="id">
       <figure class="tray-input-icon" :class="inputClass">
         <svg class="tray-icon-locked" viewBox="0 0 512 512">
-          <!-- eslint-disable-next-line -->
-          <path class="path1" d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path>
+          <path
+            class="path1"
+            d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48
+            48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104
+            0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"
+          ></path>
         </svg>
       </figure>
       <button v-show="value"
