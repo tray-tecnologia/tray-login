31f2cd63f839f6a979c114acc7068b4dceb84b31
Rayanne Bertolace Lima <rayanne.lima@tray.net.br>
2022-09-20 12:06:39 -0300

    fix: ajuste de icones, lint e ordem dos botões no app

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,61 @@
 <template>
   <div class="tray-container"
     ref="tray-login" v-show="showComponent">
-    <button class="tray-close"
-      @click="close">
-      X
+    <button class="tray-close" @click="close">
+      <icon name="close" />
     </button>
-    <app-identification id="identify" v-if="screen === 'Identification'"
+
+    <app-identification
+      v-if="screen === 'Identification'"
+      id="identify"
       class="tray-login-screens"
       :callback="this.dataCallback"
-      :params="params">
+      :params="params"
+    >
       <app-custom-texts v-if="hasCustomTexts"
         :action="this.customTexts['main-action']"
         slot="custom-texts">
       </app-custom-texts>
+
       <app-facebook-login
         v-if="facebookEnabled"
         :callback="this.dataCallback"
         :callbackPost="this.dataCallbackPost"
         :params="params"
+        label="Fazer login com o Facebook"
         slot="app-facebook-login"
       />
+
       <app-google-login
         v-if="googleEnabled"
         :callback="this.dataCallback"
         :callbackPost="this.dataCallbackPost"
         :params="params"
+        label="Fazer login com o Google"
         slot="app-google-login"
       />
     </app-identification>
 
-    <app-login id="main" v-if="screen === 'Main'"
+    <app-login
+      v-if="screen === 'Main'"
+      id="main"
       class="tray-login-screens"
       :callback="this.dataCallback"
       :callbackPost="this.dataCallbackPost"
-      :params="params">
+      :params="params"
+    >
       <app-custom-texts v-if="hasCustomTexts"
         :error="this.customTexts['general-error-alert']"
         :action="this.customTexts['main-action']"
-        slot="custom-texts">
-      </app-custom-texts>
+        slot="custom-texts"
+      />
+
       <app-otp-button v-if="otpEnabled"
         :callback="this.dataCallback"
         :params="params"
         slot="app-otp-login"
-      >
-      </app-otp-button>
+      />
+
       <app-facebook-login
         v-if="facebookEnabled"
         :callback="this.dataCallback"
@@ -52,6 +63,7 @@
         :params="params"
         slot="app-facebook-login"
       />
+
       <app-google-login
         v-if="googleEnabled"
         :callback="this.dataCallback"
@@ -59,67 +71,61 @@
         :params="params"
         slot="app-google-login"
       />
-      <button type="button"
-        v-if="identificationEnabled"
-        class="tray-btn-default tray-btn-other-option"
-        @click="reset"
-        slot="app-back-step">
-        {{ $lang['go-back'] }}
-      </button>
     </app-login>
 
-    <section id="blocked" v-if="screen === 'Blocked'"
-      class="tray-login-screens">
+    <section id="blocked" v-if="screen === 'Blocked'" class="tray-login-screens">
       <div>
         <strong class="tray-title tray-login__title">
           {{ $lang['main-title']}}
         </strong>
-        <p class="tray-action tray-error-message">
+
+        <p class="tray-action tray-warning-message">
           {{ $lang['blocked-user'] }}
         </p>
       </div>
+
       <app-custom-texts v-if="hasCustomTexts"
         :error="this.customTexts['general-error-alert']"
         :action="this.customTexts['main-action']"
-        slot="custom-texts">
-      </app-custom-texts>
+        slot="custom-texts"
+      />
+
       <p class="tray-action" v-else v-html="$lang['main-action']"></p>
-      <app-facebook-login
-        v-if="facebookEnabled"
-        :callback="this.dataCallback"
-        :callbackPost="this.dataCallbackPost"
-        :params="params"
-        slot="app-facebook-login"
-      />
-      <app-google-login
-        v-if="googleEnabled"
-        :callback="this.dataCallback"
-        :callbackPost="this.dataCallbackPost"
-        :params="params"
-        slot="app-google-login"
-      />
-      <button v-if="identificationEnabled"
-        class="tray-btn-default"
-        @click="reset"
-        slot="app-back-step">
-        {{ $lang['go-back'] }}
-      </button>
+
+      <div class="tray-social-login">
+        <app-google-login
+          v-if="googleEnabled"
+          :callback="this.dataCallback"
+          :callbackPost="this.dataCallbackPost"
+          :params="params"
+          slot="app-google-login"
+        />
+
+        <app-facebook-login
+          v-if="facebookEnabled"
+          :callback="this.dataCallback"
+          :callbackPost="this.dataCallbackPost"
+          :params="params"
+          slot="app-facebook-login"
+        />
+      </div>
     </section>
+
     <section id="tray-login-terms" v-if="hasTerm">
       <app-terms :termText="this.dataTerms"></app-terms>
     </section>
-    <section id="tray-login-loading" v-show="loading"
-      :class="{
-        'tray-loading-hidden': !loading
-      }"
-      class="tray-loading">
+
+    <section
+      v-show="loading"
+      id="tray-login-loading"
+      class="tray-loading"
+      :class="{ 'tray-loading-hidden': !loading }"
+    >
       <div class="tray-loading-mask">
         <div class="tray-loading-line"></div>
       </div>
-      <svg class="tray-loading-icon tray-icon-locked" viewBox="0 0 1024 1024">
-        <!-- eslint-disable-next-line -->
-        <path class="path1" d="M796.467 417.109v-132.642c0-155.58-128.956-284.467-284.467-284.467s-284.467 128.887-284.467 284.467v132.642c-64.444 0-113.801 49.289-113.801 113.801v379.29c0.068 64.444 49.289 113.801 113.801 113.801h568.866c64.444 0 113.801-49.289 113.801-113.801v-379.29c0.068-60.689-49.289-113.801-113.732-113.801zM265.489 284.399c0-136.533 109.978-246.511 246.511-246.511s246.511 109.978 246.511 246.511v132.71h-37.956v-132.71c0-113.801-94.822-208.623-208.623-208.623s-208.555 94.822-208.555 208.623v132.71h-37.956l0.068-132.71zM682.667 284.399v132.71h-341.333v-132.71c0-94.822 75.844-170.667 170.667-170.667s170.667 75.844 170.667 170.667zM872.311 568.798v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333v75.844h-341.333v37.956h341.333c0 41.711-34.133 75.844-75.844 75.844h-568.866c-41.711 0-75.844-34.133-75.844-75.844v-379.221c0-41.711 34.133-75.844 75.844-75.844h568.866c41.711 0 75.844 34.133 75.844 75.844h-341.333v37.956l341.333-0.137z"></path>
-      </svg>
+
+      <icon name="locked-loading" />
     </section>
   </div>
 </template>
@@ -137,6 +143,7 @@ import AppLogin from './screens/Login/screens/Main.vue';
 import AppOtpButton from './screens/Login/screens/Otp/Button.vue';
 import AppCustomTexts from './components/CustomTexts.vue';
 import AppTerms from './components/Terms.vue';
+import Icon from './components/icons/index.vue';
 
 export default {
   store,
@@ -149,6 +156,7 @@ export default {
     AppOtpButton,
     AppLogin,
     AppTerms,
+    Icon,
   },
   mixins: [screenHandler, utils],
   data() {
