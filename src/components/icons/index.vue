b53105cab893d67353e8a0e73a954cc0c40bfd18
Rayanne Bertolace Lima <rayanne.lima@tray.net.br>
2022-09-20 12:00:42 -0300

    fix: ícones antigos na nova estrutura

--- src/components/icons/index.vue
+++ src/components/icons/index.vue
@@ -10,9 +10,12 @@ import Key from './KeyIcon.vue';
 import Email from './EmailIcon.vue';
 import Document from './DocumentIcon.vue';
 import Locked from './LockedIcon.vue';
+import LockedLoading from './LockedLoadingIcon.vue';
 import Visible from './VisibleIcon.vue';
 import Hidden from './HiddenIcon.vue';
 import Check from './CheckIcon.vue';
+import Times from './TimesIcon.vue';
+import CircleBordered from './CircleBordered.vue';
 
 export default {
   name: 'IconFactory',
@@ -25,9 +28,12 @@ export default {
     Email,
     Document,
     Locked,
+    LockedLoading,
     Visible,
     Hidden,
     Check,
+    Times,
+    CircleBordered,
   },
 
   props: {
